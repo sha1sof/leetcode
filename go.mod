93cd9742c471b87097ecd5eac4bf260a58667981
sha1sof <shaisof@yandex.ru>
2024-11-11 01:18:26 +0300

    Solved 1 task

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sha1sof/leetcode
+
+go 1.23.1
---
+++ task1/task_test.go
@@ -0,0 +1,34 @@
+package task1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTask1(t *testing.T) {
+	tests := []struct {
+		money    int
+		children int
+		expected int
+	}{
+		{
+			20,
+			3,
+			1,
+		},
+		{
+			16,
+			2,
+			2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.money), func(t *testing.T) {
+			result := distMoney(tt.money, tt.children)
+			if result != tt.expected {
+				t.Errorf("got %d, want %d", result, tt.expected)
+			}
+		})
+	}
+}
